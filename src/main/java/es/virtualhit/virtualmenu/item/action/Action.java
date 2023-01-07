a89172e9498e6660b8a0660e04a58c0c541a3d8b
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-01-07 17:53:51 +0100

    Add action object

---
+++ src/main/java/es/virtualhit/virtualmenu/item/action/Action.java
@@ -0,0 +1,8 @@
+package es.virtualhit.virtualmenu.item.action;
+
+/**
+ * This class represents an action that can be executed when a player clicks on a menu item.
+ * This class should be extended to create custom actions.
+ */
+public class Action {
+}
