9a5d63047eec6195cf5e2a3bbebb9e0f5d8ebb66
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-01-07 18:15:15 +0100

    Add player click menu item event

---
+++ src/main/java/es/virtualhit/virtualmenu/event/PlayerClickMenuItemEvent.java
@@ -0,0 +1,54 @@
+package es.virtualhit.virtualmenu.event;
+
+import es.virtualhit.virtualmenu.item.MenuItem;
+import es.virtualhit.virtualmenu.menu.Menu;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Event called when a player clicks on a MenuItem item.
+ */
+public class PlayerClickMenuItemEvent extends PlayerEvent implements Cancellable {
+
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    private final Menu menu;
+    private final MenuItem item;
+    private boolean cancelled;
+
+    public PlayerClickMenuItemEvent(Player player, Menu menu, MenuItem item) {
+        super(player);
+        this.menu = menu;
+        this.item = item;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public MenuItem getItem() {
+        return item;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return false;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+}
