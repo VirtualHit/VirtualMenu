ae3b3c94e0084eae1252865508d79ebe880f025c
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-03-05 17:49:30 +0100

    Fix

--- src/main/java/es/virtualhit/virtualmenu/listener/InventoryClickListener.java
+++ src/main/java/es/virtualhit/virtualmenu/listener/InventoryClickListener.java
@@ -4,6 +4,7 @@ import es.virtualhit.virtualmenu.VirtualMenu;
 import es.virtualhit.virtualmenu.event.PlayerClickMenuItemEvent;
 import es.virtualhit.virtualmenu.menu.Menu;
 import es.virtualhit.virtualmenu.menu.item.MenuItem;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,7 +15,6 @@ public class InventoryClickListener implements Listener {
     @EventHandler
     public void onClick(InventoryClickEvent event) {
         if (!(event.getWhoClicked() instanceof Player player)) return;
-
         if (!VirtualMenu.containsPlayer(player)) return;
 
         Menu menu = VirtualMenu.getPlayerMenu(player);
@@ -25,8 +25,13 @@ public class InventoryClickListener implements Listener {
             return;
         }
 
-        PlayerClickMenuItemEvent clickEvent = new PlayerClickMenuItemEvent(menu, item);
-        item.getClickable().onClick(clickEvent);
-        event.setCancelled(clickEvent.isCancelled());
+        try {
+            PlayerClickMenuItemEvent clickEvent = new PlayerClickMenuItemEvent(menu, item);
+            item.getClickable().onClick(clickEvent);
+            event.setCancelled(clickEvent.isCancelled());
+        } catch (Exception e) {
+            event.setCancelled(true);
+            Bukkit.getLogger().warning("Un plugin que usa VirtualMenu ha causado un error al ejecutar el evento de click.");
+        }
     }
 }
